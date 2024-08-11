090ad8a23790ffee9d3e81a9e0d3f5f499a8e610
yashitiwari06 <yashitiwari2006@gmail.com>
2024-08-11 23:01:34 +0530

    Another change in main branch

--- A stone on the Table.py
+++ A stone on the Table.py
@@ -2,3 +2,5 @@ n = int(input("the number of stones on the table"))
 s = input("Color of stone:") 
 if (s.count()==n):
     print('No. of stones to be taken out ',n-1)
+
+# This commit is in main branch
